2b30023a11cffe889cc0b5b671feb6707fa38434
Nunko <nunnunkoip@gmail.com>
2022-10-01 02:57:24 +0900

    함수 추가

--- Assets/Scripts/Camera/Touch.cs
+++ Assets/Scripts/Camera/Touch.cs
@@ -4,6 +4,42 @@ namespace Fruit.Camera
 {
     public class Touch : MonoBehaviour
     {
+        public GameObject lookPoint;
 
+        public float rotateSpeed;
+        
+        float xRotateMove;
+        float yRotateMove;
+        Vector2 touchPoint;
+
+        UnityEngine.Camera mainCamera;
+        
+        void Awake()
+        {
+            mainCamera = UnityEngine.Camera.main;
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0)) touchPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+            if (Input.GetMouseButton(0))
+            {
+                RotateCamera();
+            }
+        }
+
+        void RotateCamera()
+        {
+            xRotateMove = Input.GetAxis("Mouse X") * Time.deltaTime * rotateSpeed;
+            yRotateMove = Input.GetAxis("Mouse Y") * Time.deltaTime * rotateSpeed;
+
+            Vector3 lookPointPositon = lookPoint.transform.position;
+
+            mainCamera.transform.RotateAround(lookPointPositon, Vector3.right, -yRotateMove);
+            mainCamera.transform.RotateAround(lookPointPositon, Vector3.up, xRotateMove);
+
+            mainCamera.transform.LookAt(lookPointPositon);
+        }
     }
 }
