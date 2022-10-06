b79c48cd0fb34ce9f8972cbcb47d8d62964bdd1d
Nunko <nunnunkoip@gmail.com>
2022-10-06 23:50:11 +0900

    추가 테스트 코드

--- Assets/Scripts/UIF/MainUI.cs
+++ Assets/Scripts/UIF/MainUI.cs
@@ -5,9 +5,10 @@ using Fruit.Map;
 namespace Fruit.UIF
 {
     public class MainUI : MonoBehaviour 
-    {
-          
-
-        
+    {          
+        public void ClickMenuButton()
+        {
+            Debug.Log("우측 상단 버튼 클릭");
+        }        
     }
 }
