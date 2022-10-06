f35ee6e885667225730b35f7e647379d514352fd
Nunko <nunnunkoip@gmail.com>
2022-10-06 21:42:06 +0900

    함수 삭제

--- Assets/Scripts/UIF/MainUI.cs
+++ Assets/Scripts/UIF/MainUI.cs
@@ -6,17 +6,8 @@ namespace Fruit.UIF
 {
     public class MainUI : MonoBehaviour 
     {
-        public TimeEventHub _TimeEventHub;
-        public GameObject timeBackImage;
-        
-        void OnEnable()
-        {
-            _TimeEventHub.TransfortTimeEvent(TimeEventType.EVERYHOUR, FillTimeBackImage);
-        }      
+          
 
-        void FillTimeBackImage()
-        {
-            timeBackImage.GetComponent<Image>().fillAmount = (float) _TimeEventHub.currentH%12/12;
-        }
+        
     }
 }
