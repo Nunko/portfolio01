a6fcdc0478827e373b46712081f36dfff0e29499
Nunko <nunnunkoip@gmail.com>
2022-10-09 03:22:19 +0900

    함수 추가

--- Assets/Scripts/Map/PaintTrigger.cs
+++ Assets/Scripts/Map/PaintTrigger.cs
@@ -1,30 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Fruit.Behaviour;
+using Fruit.CameraM;
 
 namespace Fruit.Map
 {
     public class PaintTrigger : MonoBehaviour
     {
-        public GameObject Sign;
+        public Object Sign;
+        GameObject _Sign;
         public GameObject PaintPanel;
+        public CameraMode_SeeingSomething _seeingSomething;
 
         void OnTriggerEnter(Collider other) 
         {
+            if (!_Sign) GenerateSign();
+
             if (other.gameObject.CompareTag("Player") == true)
             {
                 InteractionEventBus.Subscribe(InteractionEventType.SEEINGPAINT, TogglePaintPanel);
-                Sign.SetActive(true);
+                CheckPaintGameObject();
+                _Sign.SetActive(true);
             }        
         }
 
         void OnTriggerStay(Collider other) 
         {
-            if (other.gameObject.CompareTag("Player") == true && Sign.activeSelf == false)
+            if (!_Sign) GenerateSign();
+
+            if (other.gameObject.CompareTag("Player") == true && _Sign.activeSelf == false)
             {
                 InteractionEventBus.Subscribe(InteractionEventType.SEEINGPAINT, TogglePaintPanel);
-                Sign.SetActive(true);
+                CheckPaintGameObject();
+                _Sign.SetActive(true);
             }        
         }
 
@@ -33,8 +41,16 @@ namespace Fruit.Map
             if (other.gameObject.CompareTag("Player") == true)
             {
                 InteractionEventBus.Unsubscribe(InteractionEventType.SEEINGPAINT, TogglePaintPanel);
-                Sign.SetActive(false);
+                _Sign.SetActive(false);
             }        
+        }
+
+        void GenerateSign()
+        {            
+            var parentTransform = gameObject.transform.parent;
+            Vector3 addPostion = new Vector3(0, 0.75f, 0);
+            _Sign = Instantiate(Sign, parentTransform) as GameObject;
+            _Sign.transform.position = parentTransform.position + addPostion;
         }
 
         void TogglePaintPanel()
@@ -47,6 +63,11 @@ namespace Fruit.Map
             {
                 PaintPanel.SetActive(false);
             }
+        }
+
+        void CheckPaintGameObject()
+        {
+            _seeingSomething.SetLookAtGObj(gameObject);
         }
     }
 }
