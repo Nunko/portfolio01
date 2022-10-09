415eff15f628fa5201980a347828bb65e038f84f
Nunko <nunnunkoip@gmail.com>
2022-10-09 19:54:56 +0900

    함수 추가

--- Assets/Scripts/UIF/PaintPanel.cs
+++ Assets/Scripts/UIF/PaintPanel.cs
@@ -6,17 +6,39 @@ namespace Fruit.UIF
     public class PaintPanel : MonoBehaviour
     {
         public CameraController _cameraControllaer;
+        public GameObject paintBoard;
+        public Camera subCamera;
 
         void OnEnable() 
         {
             Time.timeScale = 0;
-            _cameraControllaer.ChangeCameraModeToSeeingSomething();
+            _cameraControllaer.ChangeCameraModeToSeeingSomething();           
+            ShowPaintPanel();            
         }
 
         void OnDisable() 
         {
-            Time.timeScale = 1;
-            _cameraControllaer.ChangeCameraModeToAdventure();
+            HidePaintPanel();                     
+            _cameraControllaer.ChangeCameraModeToAdventure();      
+            Time.timeScale = 1;         
+        }
+
+        void ShowPaintPanel()
+        {
+            gameObject.GetComponent<CanvasGroup>().blocksRaycasts = true;
+            gameObject.GetComponent<CanvasGroup>().alpha = 1;
+            gameObject.GetComponent<CanvasGroup>().interactable = true;
+            paintBoard.SetActive(true);            
+            subCamera.enabled = true;
+        }
+
+        void HidePaintPanel()
+        {
+            if (subCamera) subCamera.enabled = false;            
+            if (paintBoard) paintBoard.SetActive(false);
+            gameObject.GetComponent<CanvasGroup>().blocksRaycasts = true;
+            gameObject.GetComponent<CanvasGroup>().alpha = 1;
+            gameObject.GetComponent<CanvasGroup>().interactable = true;
         }
     }
 }
