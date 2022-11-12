1364dd3ed7c1e7a0e018ba7a0c6976d1b132a10a
Nunko <nunnunkoip@gmail.com>
2022-11-12 13:47:47 +0900

    추가 기능5일부

--- Assets/01.Scripts/UIF/RequestPanel.cs
+++ Assets/01.Scripts/UIF/RequestPanel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using Fruit.CameraM;
 
 namespace Fruit.UIF
@@ -10,7 +11,7 @@ namespace Fruit.UIF
         public CameraController _cameraControllaer;
 
         public GameObject buttonPanel;
-        public GameObject keyboardPanel;
+        public GameObject keyboardPanel;        
 
         void OnEnable()
         {
@@ -20,6 +21,9 @@ namespace Fruit.UIF
 
         void OnDisable()
         {
+            if (keyboardPanel.activeSelf == true) ToggleKeyboardPanel();
+            if (buttonPanel.activeSelf == false) ToggleButtonPanel();
+
             _cameraControllaer.ChangeCameraModeToAdventure();
             Time.timeScale = 1;
         }
@@ -43,7 +47,27 @@ namespace Fruit.UIF
 
         void ToggleKeyboardPanel()
         {
+            inputTMPro.text = "";
             keyboardPanel.SetActive(!keyboardPanel.activeSelf);
+        }
+
+        public TextMeshProUGUI inputTMPro;
+        public string playerInputWord;
+        public void AddLetter(string alphabet)
+        {
+            inputTMPro.text += alphabet;            
+        }
+
+        public void DeleteLetter()
+        {
+            inputTMPro.text = inputTMPro.text.Remove(inputTMPro.text.Length - 1, 1);
+        }
+
+        public void SubmitWord()
+        {
+            playerInputWord = inputTMPro.text;
+            inputTMPro.text = "";
+            gameObject.SetActive(false);
         }
     }
 }
